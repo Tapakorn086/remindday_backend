7ac5cdaf3b0f4b099daad18f73b2c64affdca216
Tapakorn086 <nakason_t@silpakorn.edu>
2024-10-06 22:06:37 +0700

    update: generate function getId for return login_id

--- src/main/java/com/example/todoapp/model/Login.java
+++ src/main/java/com/example/todoapp/model/Login.java
@@ -37,4 +37,12 @@ public class Login {
     private User user;
 
     // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
