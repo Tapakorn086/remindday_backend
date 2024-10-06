249cf560c6afa2fe479bc5d93be1887871a4ea14
Tapakorn086 <nakason_t@silpakorn.edu>
2024-10-06 17:05:26 +0700

    delete: DemoApp

--- src/main/java/com/example/demo/repository/DailyDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.DailyDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DailyDetailRepository extends JpaRepository<DailyDetail, Long> {
-}
